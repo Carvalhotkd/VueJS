7b295b07024daee29e3fefbd565835a6c8151595
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-05-02 20:27:25 -0400

    Finalizado versão web e versão component

--- VueJS-em-uma-hora/segundo-projeto/vuejs-cli/src/App.vue
+++ VueJS-em-uma-hora/segundo-projeto/vuejs-cli/src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Comment />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Comment from "./components/Comment";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Comment
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
