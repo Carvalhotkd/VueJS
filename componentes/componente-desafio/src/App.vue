08c7c8794364d76a838dda70587bdb1c8e24f747
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-05-06 18:02:18 -0400

    organizando projeto em componentes

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-app>
+		<app-menu />
+		<v-toolbar app>
+			<app-toolbar />
+		</v-toolbar>
+
+		<v-content>
+			<app-carousel />
+		</v-content>		
+		<app-footer />
+	</v-app>
+</template>
+
+<script>
+import Carousel from './Carousel.vue';
+import Footer from './Footer.vue';
+import Toolbar from './Toolbar.vue';
+import Menu from './Menu.vue';
+export default {
+	components: {'app-footer': Footer, 'app-carousel': Carousel, 'app-toolbar': Toolbar, 'app-menu': Menu}
+
+}
+</script>
